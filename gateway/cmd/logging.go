9119d8ed96d4e675e2e2947cc2e101a4c6eb8c69
Subham Thapa <subhamits4.2@gmail.com>
2026-03-19 11:02:41 +0545

    logging

--- gateway/cmd/logging.go
+++ gateway/cmd/logging.go
@@ -7,7 +7,7 @@ import (
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("[%s] %s", r.Method, r.URL)
+		log.Printf("[%s] %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
 }
